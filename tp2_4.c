90c1234416797802fe99f1e3b5f3cdb8bc9d2535
BaltazarGonzalez <alejobaltazargonzalez@gmail.com>
2025-04-07 19:44:27 -0300

    funcion para mostrar

--- tp2_4.c
+++ tp2_4.c
@@ -9,6 +9,8 @@ struct compu{
 };
 typedef struct compu Compu;
 
+void listarPc(Compu pc[],int cant);
+
 int main() {
     
     srand(time(NULL));
@@ -16,11 +18,13 @@ int main() {
     int num;
     char tipos[6][10] = {"Intel", "AMD", "Celeron", "Athlon", "Core",
 "Pentium"};
+
+
    
     Compu pcs[5];
     for(int i=0;i<5;i++){
         pcs[i].velocidad = 1+rand()%3;
-        pcs[i].anio = 2015+rand()%2024;
+        pcs[i].anio = (rand()%(2024-2015+1)+2015);
         pcs[i].cNucleos = 1+rand()%8;
         num = rand()%5;
         pcs[i].tipoCpu = tipos[num];
@@ -28,3 +32,8 @@ int main() {
     }
     return 0;
     }
+void listarPc(Compu pc[],int cant){
+    for(int a=0;a<cant;a++){
+        printf("velocidad: %d año: %d  nucleos: %d  Procesador: %s\n",pc[a].velocidad,pc[a].anio,pc[a].cNucleos,pc[a].tipoCpu);
+    }
+}        
